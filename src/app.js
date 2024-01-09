e6531ae638b05fb71f9397572fea29a58bdaa39a
Jubayer Abdullah Joy <jubayerjoy98@gmail.com>
2024-01-09 18:52:50 +0600

    feat(swagger): add open api 3 spec for all endpoint
    Signed-off-by: Jubayer Abdullah Joy <jubayerjoy98@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,26 +1,20 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+
 const bodyParser = require("body-parser");
 const { sequelize } = require("./model");
 const { authenticateUser } = require("./middleware/authentication");
 const routes = require("./routes");
+const swaggerDocument = require("../docs/swagger.json");
 
 const app = express();
 app.use(bodyParser.json());
 app.set("sequelize", sequelize);
 app.set("models", sequelize.models);
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(authenticateUser);
 app.use(routes);
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-// app.get("/contracts/:id", async (req, res) => {
-//   const { Contract } = req.app.get("models");
-//   const { id } = req.params;
-//   const contract = await Contract.findOne({ where: { id } });
-//   if (!contract) return res.status(404).end();
-//   res.json(contract);
-// });
 module.exports = app;
